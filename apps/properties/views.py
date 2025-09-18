09831f08c3a92a33e1f0401158c08013e636e7b7
Muhammad Fazeel Rafiq <malikfazeel@gmail.com>
2025-09-19 00:36:33 +0500

    fix properties view

--- apps/properties/views.py
+++ apps/properties/views.py
@@ -1,15 +1,23 @@
 from rest_framework import viewsets, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Property
 from .serializers import PropertySerializer
+from .filters import PropertyFilter
 
 
 class PropertyViewSet(viewsets.ModelViewSet):
     serializer_class = PropertySerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = PropertyFilter
 
     def get_queryset(self):
-        # only show properties of the logged-in user
-        return Property.objects.filter(owner=self.request.user)
+        user = self.request.user
+        if user.is_staff or user.is_superuser:
+            # Staff/admin can see all properties
+            return Property.objects.all()
+        # Normal users only see their own
+        return Property.objects.filter(owner=user)
 
     def perform_create(self, serializer):
         # assign logged-in user as the owner
