a86702ed63d97b0e40a5c6f6ce5973502195b7ee
Muhammad Fazeel Rafiq <malikfazeel@gmail.com>
2025-09-19 00:50:08 +0500

    fix cors issue

--- asset_management/settings.py
+++ asset_management/settings.py
@@ -61,7 +61,8 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
