ecaf86e04351183cbd94d7ece8ac8a707d45ee04
Muhammad Fazeel Rafiq <malikfazeel@gmail.com>
2025-09-19 18:04:23 +0500

    remove unnecessary field role

--- apps/authentication/managers.py
+++ apps/authentication/managers.py
@@ -14,7 +14,6 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("role", "super_admin")
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser must have is_staff=True.")
