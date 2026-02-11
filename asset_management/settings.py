4879b3fab4b139bd666ac3c69154c494e5092f32
Muhammad Fazeel Rafiq <malikfazeel@gmail.com>
2026-02-11 21:47:48 +0500

    remove unnecessary code

--- asset_management/settings.py
+++ asset_management/settings.py
@@ -18,10 +18,6 @@ from corsheaders.defaults import default_headers
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# laod .env
-from dotenv import load_dotenv
-load_dotenv(BASE_DIR / ".env")
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
